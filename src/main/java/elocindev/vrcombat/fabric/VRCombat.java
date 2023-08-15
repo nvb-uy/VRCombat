08de08790f565412ddff021f237cf189eac2331a
ThisMight <stijn.naivin@hotmail.com>
2023-08-15 20:23:45 +0200

    1.20.1 - The ACTUAL fixup

--- src/main/java/elocindev/vrcombat/fabric/VRCombat.java
+++ src/main/java/elocindev/vrcombat/fabric/VRCombat.java
@@ -7,24 +7,26 @@ import org.slf4j.LoggerFactory;
 
 import elocindev.vrcombat.fabric.config.ConfigBuilder;
 import elocindev.vrcombat.fabric.config.ConfigEntries;
+import org.vivecraft.client.VivecraftVRMod;
 import org.vivecraft.client_vr.VRState;
+import org.vivecraft.fabric.VivecraftMod;
 
 public class VRCombat implements ClientModInitializer {
     public static final String MODID = "vr-combat";
 	public static final Logger LOGGER = LoggerFactory.getLogger("vr-combat");
 	public static ConfigEntries CONFIG;
-	public static boolean isVR = false;
+
+	//Removed the boolean, since this version has hotswapping.
 
 	@Override
 	public void onInitializeClient() {
 		CONFIG = ConfigBuilder.loadConfig();
 		LOGGER.info("VRCombat Config Initialized!");
 
-		isVR = VRState.vrEnabled;
-		if (isVR) {
-			LOGGER.info("VRCombat detected Vivecraft VR Mode!");
+		if (VRState.vrInitialized) {
+			LOGGER.info("VRCombat detected Vivecraft VR Mode, May change later!");
 		} else {
-			LOGGER.info("VRCombat detected Vivecraft NONVR Mode!");
+			LOGGER.info("VRCombat detected Vivecraft NONVR Mode, May change later!");
 		}
 	}
 }
